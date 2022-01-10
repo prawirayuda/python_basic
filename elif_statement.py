11da0c2f2b835ee8052511168cd70cee5b0070b4
prawirayuda <muhammadprawirayuda@gmail.com>
2022-01-10 15:25:39 +0700

    elif statement

--- elif_statement.py
+++ elif_statement.py
@@ -1,10 +1,26 @@
-nama = input("nama ?")
+# nama = input("nama ?")
 
-if nama == "sup":
-    print ("oke")
-elif nama =="bo":
-    print("bo")
-else:
-    print("au ah")
+# if nama == "sup":
+#     print ("oke")
+# elif nama =="bo":
+#     print("bo")
+# else:
+#     print("au ah")
 
-print("end")
+# print("end")
+
+##latihan kalkulator lagee 
+
+# angka1 = float (input ("angka 1 : "))
+# operator = input("-/+/*/bagi")
+# angka2 = float (input ("angka 2 : "))
+
+# if operator == "-":
+#     hasil = angka1-angka2
+#     print(f"Hasil nya : {hasil}")
+# elif 
+
+import camelcase 
+c = camelcase.CamelCase()
+txt = "In"
+print(c.hump(txt))
