bd00ebdcb2edc1cab7da9ed5fd5b37f58833446d
prawirayuda <muhammadprawirayuda@gmail.com>
2022-01-10 14:41:20 +0700

    if else

---
+++ if-else.py
@@ -0,0 +1,25 @@
+nama = input("siapa anda? :\n")
+# print =(nama)
+
+# if kondisi: aksi
+
+#if inline 
+# if nama == "yud" : print("oke{nama}")
+# print("langsung pass{nama}")
+
+#if indentation
+
+
+# if nama == "ucup":
+#     print("iya sup")
+    
+# print(f"Keluar {nama}")
+
+
+#if else 
+if nama == "yud":
+    print("oke")
+else: 
+    print("not yud")
+    
+print("end")
