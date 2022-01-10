5e9ddaa065ee61b41800ae7c93197f4d86696374
prawirayuda <muhammadprawirayuda@gmail.com>
2022-01-10 15:38:31 +0700

    for loop

---
+++ for_loop.py
@@ -0,0 +1,13 @@
+# angka = [0,1,2,3,4,4]
+
+# for i in angka:
+#     print(i)
+
+
+# angka2_range = range(1,9) #bisa range(4)
+# for i in angka2_range:
+#     print("haras")
+
+data_str = "asfsdfsadfsdf"
+for letter in data_str:
+    print(letter)
